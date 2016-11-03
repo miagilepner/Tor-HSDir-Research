d2c1a98168554c4270cb97d3b1824d5f1faee106
miagilepner <miagilepner@berkeley.edu>
2016-11-03 15:36:29 -0700

    added DonnaC code and modified it for python3

--- hsdir-calc.py
+++ hsdir-calc.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 from stem.descriptor import parse_file
+from stem.descriptor.reader import DescriptorReader
 from stem import Flag
 import os
 import binascii 
 import bisect 
-
+import calc_ids
 #returns a list of the 3 hsdirs closest to but larger than digest
 def getDirs(digest, hsdirs_sorted, hsdirs_keys):
   dirlist = []
@@ -24,8 +26,8 @@ def getDirs(digest, hsdirs_sorted, hsdirs_keys):
       dirlist.append(hsdirs_sorted[1])
   else:
     if digest_one > hsdirs_keys[hsdir_size-1]:
-      dirlist.append(hsdirs_sorted[0]
-      dirlist.append(hsdirs_sorted[1]
+      dirlist.append(hsdirs_sorted[0])
+      dirlist.append(hsdirs_sorted[1])
       dirlist.append(hsdirs_sorted[2])
     elif digest_one == hsdirs_keys[hsdir_size-1]:
       dirlist.append(hsdirs_sorted[hsdir_size-1])
@@ -33,11 +35,11 @@ def getDirs(digest, hsdirs_sorted, hsdirs_keys):
       dirlist.append(hsdirs_sorted[1])
   return dirlist 
 
-#for filename
 #finds 3 HSDirs for digest_one and 3 for digest_two
 #outputs all details for these HSDirs
 def analyzeHSDirs(filename, digest_one, digest_two):
   dirlist = []
+  #descriptors = DescriptorReader("../old_consensus/%s" % filename)
   descriptors = parse_file('../old_consensus/%s' % filename) 
   hsdirs = filter(lambda descriptor:Flag.HSDIR in descriptor.flags, descriptors)
   hsdirs_sorted = sorted(hsdirs, key=lambda descriptor:binascii.unhexlify(descriptor.digest),reverse=True)
@@ -64,4 +66,15 @@ def createJSON(dirlist):
       item_dict['flags'].append(flag) 
     item_dict['version_line'] = item.version_line 
     json_data[i] = item_dict
-  return json_data 
+  return json_data
+
+#because of the way Stem works, we can only look at consensuses for months at a time
+def run(onion_address):
+  daysPast = 3 
+  digests = calc_ids.findDigests(onion_address, days=daysPast, past=True)
+  for digest in digests:
+    print(digest)
+    desc_ids = digest[1]
+    #analyzeHSDirs(,desc_ids[0],desc_ids[1]) 
+
+run("3g2upl4pq6kufc4m.onion")
